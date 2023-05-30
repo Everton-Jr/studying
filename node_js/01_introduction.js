f1e8db92576e81fdb6c9daf0c23d1eda454e3945
Ever <evertonxdoj@gmail.com>
2023-05-30 07:35:30 -0300

    function added

---
+++ node_js/01_introduction.js
@@ -0,0 +1,9 @@
+// if you type node this_name_file.js on the terminal, you can see the result
+
+function SayMyName(name) {
+    console.log(`Hello ${name}`)
+}
+
+SayMyName('Ever')
+
+console.log("Hello World")
